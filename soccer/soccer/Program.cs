17234e0b0459b1306b271af445d81b77b6614fed
justin <justinzhang515@gmail.com>
2023-10-03 15:05:19 +0800

    Added Swagger and Hangfire package

--- soccer/soccer/Program.cs
+++ soccer/soccer/Program.cs
@@ -1,7 +1,26 @@
+using Hangfire;
+using Hangfire.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add hangfire to the container.
+builder.Services.AddHangfire(configuration => configuration
+       .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+       .UseSimpleAssemblyNameTypeSerializer()
+       .UseRecommendedSerializerSettings()
+       .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+       {
+           CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+           SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+           QueuePollInterval = TimeSpan.Zero,
+           UseRecommendedIsolationLevel = true,
+           DisableGlobalLocks = true
+       }));
+builder.Services.AddHangfireServer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -13,10 +32,20 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+// use swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseHangfireDashboard("/dashboard");
 
 app.UseAuthorization();
 
