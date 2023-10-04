542fbda9833f5ae32d39611aa573c163e940e361
justin <justinzhang515@gmail.com>
2023-10-04 18:23:12 +0800

    Added MatchResultDb Project and three table (MatchResult, MatchDetail, History)

---
+++ soccer/MatchResultDb/dbo/Tables/History.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE History
+(
+	ResultId varchar(80) NOT NULL,
+	FirstHalf_H int,
+	FirstHalf_A int,
+	SecondHalf_H int,
+	SecondHalf_A int,
+	RegularTime_H int,
+	RegularTime_A int,
+	Corners_H int,
+	Corners_A int,
+	Penalties_H int,
+	Penalties_A int,
+	YellowCards_H int,
+	YellowCards_A int,
+	RedCards_H int,
+	RedCards_A int,
+	FirstET_H int,
+	FirstET_A int,
+	SecondET_H int,
+	SecondET_A int,
+	PenaltiesShootout_H int,
+	PenaltiesShootout_A int
+);
---
+++ soccer/MatchResultDb/dbo/Tables/MatchResult.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE MatchResult
+(
+    Id varchar(80) NOT NULL PRIMARY KEY,
+	GameTime DateTime NOT NULL,
+	Leagues varchar(100) NOT NULL,
+	HomeTeam varchar(50) NOT NULL,
+	AwayTeam varchar(50) NOT NULL,
+	HomeScore int,
+	AwayScore int,
+	Condition int --1:normal, 0:cancelled, 2:notStart
+);
