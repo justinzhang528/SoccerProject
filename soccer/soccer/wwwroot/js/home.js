7e585394ca3f3cb92e4c4dcb54937b28cc9ff416
justin <justinzhang515@gmail.com>
2023-10-05 10:32:11 +0800

    removed unused code

--- soccer/soccer/wwwroot/js/home.js
+++ soccer/soccer/wwwroot/js/home.js
@@ -1,21 +1,3 @@
-//document.addEventListener('DOMContentLoaded', function () {
-//    const toggleButtons = document.querySelectorAll('.toggle-detail');
-
-//    toggleButtons.forEach(button => {
-//        button.addEventListener('click', function () {
-//            const detailTable = this.nextElementSibling;
-
-//            if (detailTable.style.display === 'none' || detailTable.style.display === '') {
-//                detailTable.style.display = 'block';
-//                this.innerText = 'Hide Detail';
-//            } else {
-//                detailTable.style.display = 'none';
-//                this.innerText = 'Show Detail';
-//            }
-//        });
-//    });
-//});
-
 function detailEvent(e) {
     detailTrId = "#detail_" + e.id;
     if (e.innerText === 'Show Details') {
