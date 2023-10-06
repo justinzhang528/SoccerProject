43aa8cce71ee7c29fb7f4046bb82421e56361283
justin <justinzhang515@gmail.com>
2023-10-06 11:56:15 +0800

    Finished Show Detailed button function (show detail with bootstrap modal)

--- soccer/Soccer.Web/wwwroot/js/home.js
+++ soccer/Soccer.Web/wwwroot/js/home.js
@@ -1,12 +1,32 @@
-function detailEvent(e) {
-    detailTrId = "#detail_" + e.id;
-    if (e.innerText === 'Show Details') {
-        e.innerText = 'Hide Details'
-        $(detailTrId).css('display','block')
-    } else {
-        e.innerText = 'Show Details'
-        $(detailTrId).css('display', 'none')
-
-    }
+function getDetailById(e) {
+    $.ajax({
+        url: '/detail?id=' + e.id,
+        type: 'GET',
+        success: function (data) {
+            console.log(data)
+            $('#firstHalf_H').text(data.firstHalf_H);
+            $('#firstHalf_A').text(data.firstHalf_A);
+            $('#secondHalf_H').text(data.secondHalf_H);
+            $('#secondHalf_A').text(data.secondHalf_A);
+            $('#firstHalf_A').text(data.firstHalf_A);
+            $('#firstHalf_A').text(data.firstHalf_A);
+            $('#regularTime_H').text(data.regularTime_H);
+            $('#regularTime_A').text(data.regularTime_A);
+            $('#corners_H').text(data.corners_H);
+            $('#corners_A').text(data.corners_A);
+            $('#penalties_H').text(data.penalties_H);
+            $('#penalties_A').text(data.penalties_A);
+            $('#yellowCards_H').text(data.yellowCards_H);
+            $('#yellowCards_A').text(data.yellowCards_A);
+            $('#redCards_H').text(data.redCards_H);
+            $('#redCards_A').text(data.redCards_A);
+            $('#firstET_H').text(data.firstET_H);
+            $('#firstET_A').text(data.firstET_A);
+            $('#secondET_H').text(data.secondET_H);
+            $('#secondET_A').text(data.secondET_A);
+            $('#penaltiesShootout_H').text(data.penaltiesShootout_H);
+            $('#penaltiesShootout_A').text(data.penaltiesShootout_A);
+        }
+    });
 }
 
