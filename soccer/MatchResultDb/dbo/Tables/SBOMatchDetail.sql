1f6a9abcb375e4ab2932f89b003b27bf00fd3e4b
justin <justinzhang515@gmail.com>
2023-10-11 17:26:14 +0800

    Added two tables

---
+++ soccer/MatchResultDb/dbo/Tables/SBOMatchDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SBOMatchDetail]
+(
+	Id varchar(80) NOT NULL PRIMARY KEY,
+	GoalType varchar(100) NOT NULL,
+	FirstHalfScore int,
+	FullTimeScore int,
+	Code int,
+	UpdateTime DATETIME
+)
---
+++ soccer/MatchResultDb/dbo/Tables/SBOMatchResult.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[SBOMatchResult]
+(
+	Id varchar(80) NOT NULL PRIMARY KEY,
+	Leagues varchar(100) NOT NULL,
+	TeamA varchar(50) NOT NULL,
+	TeamB varchar(50) NOT NULL,
+	GameTime DateTime NOT NULL,
+	FirstHalfScore int,
+	FullTimeScore int,
+	IsShowMoreData int,
+	UpdateTime DATETIME
+)
