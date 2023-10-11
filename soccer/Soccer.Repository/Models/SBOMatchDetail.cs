4442ba2e5f438ec997818b07b61a8605653af7fa
justin <justinzhang515@gmail.com>
2023-10-11 16:26:23 +0800

    Added SBOMatchDetail class

---
+++ soccer/Soccer.Repository/Models/SBOMatchDetail.cs
@@ -0,0 +1,22 @@
+﻿namespace Soccer.Repository.Models
+{
+    public class SBOMatchDetail
+    {
+        public string Id { get; set; }
+        public string GoalType { get; set; }
+        public string FirstHalfScore { get; set; }
+        public string FullTimeScore { get; set; }
+        public string Code { get; set; }
+
+        public SBOMatchDetail() { }
+
+        public SBOMatchDetail(string id, string goalType, string firstHalfScore, string fullTimeScore, string code)
+        {
+            Id = id;
+            GoalType = goalType;
+            FirstHalfScore = firstHalfScore;
+            FullTimeScore = fullTimeScore;
+            Code = code;
+        }
+    }
+}
