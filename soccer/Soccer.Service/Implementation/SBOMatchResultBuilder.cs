d72174cc368cfc171e144f888ec1d352c2c97ec1
justin <justinzhang515@gmail.com>
2023-10-11 17:20:43 +0800

    finished generate SBOMatchResult and SBOMatchDetail function

--- soccer/Soccer.Service/Implementation/SBOMatchResultBuilder.cs
+++ soccer/Soccer.Service/Implementation/SBOMatchResultBuilder.cs
@@ -1,24 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Soccer.Repository.Models;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace Soccer.Service.Implementation
 {
     public class SBOMatchResultBuilder
     {
-        private async Task<string> GetCrawlingContentFromNetAsync()
+        private string _url;
+
+        public void SetUrl(string url)
         {
-            string res = "";
+            _url = url;
+        }
+
+        public async Task<List<SBOMatchResultModel>> GenerateSBOMatchResultsAsync()
+        {
+            List<SBOMatchResultModel> sBOMatchResultModels = new List<SBOMatchResultModel>();
             // Create an HttpClient with a CookieContainer to manage cookies
             using (var handler = new HttpClientHandler())
             {
                 using (var client = new HttpClient(handler))
                 {
                     // Set the URL of the website you want to crawl
-                    string url = "https://sports-demo-wl.17mybet.com/web-root/restricted/result/results-data.aspx";
                     // Create a CookieContainer and add the session cookie
                     var cookies = new CookieContainer();
                     var cookie = new Cookie("ASP.NET_SessionId", "rqlbxric0qwgm4m1nlxjiowx");
@@ -28,7 +32,8 @@ namespace Soccer.Service.Implementation
                     handler.CookieContainer = cookies;
 
                     // Define the payload data
-                    var postData = new FormUrlEncodedContent(new[]
+                    FormUrlEncodedContent postData;
+                    postData = new FormUrlEncodedContent(new[]
                     {
                         new KeyValuePair<string, string>("result-token", "1,1,1"),
                         new KeyValuePair<string, string>("actionValue", "today"),
@@ -37,27 +42,117 @@ namespace Soccer.Service.Implementation
                         new KeyValuePair<string, string>("names", ""),
                         new KeyValuePair<string, string>("isMobile", "0"),
                         new KeyValuePair<string, string>("HidCK", "uJdZa")
+
                     });
 
-                    // Send an HTTP GET request
-                    var response = await client.PostAsync(url, postData);
+                    // Send an HTTP POST request
+                    var response = await client.PostAsync(_url, postData);
 
                     if (response.IsSuccessStatusCode)
                     {
                         // Read the response content
-                       res = await response.Content.ReadAsStringAsync();
-                        
+                        string content = await response.Content.ReadAsStringAsync();
+
+                        if (content != null)
+                        {
+                            string pattern = @"<script>(.*?)</script>";
+                            MatchCollection matches = Regex.Matches(content, pattern);
+
+                            if (matches.Count > 0)
+                            {
+                                string resultString = matches[matches.Count - 1].Groups[1].Value;
+                                resultString = Regex.Match(resultString, @"\[.*\]").Value;
+                                resultString = resultString.Replace("\\x", "");
+                                List<object>? listResult = JsonConvert.DeserializeObject<List<object>>(resultString);
+                                foreach (var result in listResult)
+                                {
+                                    List<object> list = JsonConvert.DeserializeObject<List<object>>(result.ToString());
+                                    string id = list.ElementAt(0).ToString();
+                                    string league = list.ElementAt(1).ToString();
+                                    string teamA = list.ElementAt(2).ToString();
+                                    string teamB = list.ElementAt(3).ToString();
+                                    string dateTime = list.ElementAt(4).ToString();
+                                    string isShowMore = list.ElementAt(7).ToString();
+                                    string firstHalfScore = list.ElementAt(10).ToString();
+                                    string fullTimeScore = list.ElementAt(11).ToString();
+                                    SBOMatchResultModel model = new SBOMatchResultModel(id, league, teamA, teamB, dateTime, firstHalfScore, fullTimeScore, isShowMore);
+                                    sBOMatchResultModels.Add(model);
+                                }
+                            }
+                        }
+
                     }
                 }
             }
-
-            return res;
+            return sBOMatchResultModels;
         }
 
-        public Task<string> GetString()
+        public async Task<List<SBOMatchDetailModel>> GenerateSBOMatchDetailsAsync(string eventId)
         {
-            var res = GetCrawlingContentFromNetAsync();
-            return res;
+            List<SBOMatchDetailModel> sBOMatchDetailModels = new List<SBOMatchDetailModel>();
+            // Create an HttpClient with a CookieContainer to manage cookies
+            using (var handler = new HttpClientHandler())
+            {
+                using (var client = new HttpClient(handler))
+                {
+                    // Set the URL of the website you want to crawl
+                    // Create a CookieContainer and add the session cookie
+                    var cookies = new CookieContainer();
+                    var cookie = new Cookie("ASP.NET_SessionId", "rqlbxric0qwgm4m1nlxjiowx");
+                    cookie.Domain = "sports-demo-wl.17mybet.com";
+                    cookie.Path = "/";
+                    cookies.Add(cookie);
+                    handler.CookieContainer = cookies;
+
+                    // Define the payload data
+                    FormUrlEncodedContent postData;
+                    postData = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("eventId", eventId)
+
+                    });
+
+                    // Send an HTTP POST request
+                    var response = await client.PostAsync(_url, postData);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Read the response content
+                        string content = await response.Content.ReadAsStringAsync();
+
+                        if (content != null)
+                        {
+                            string pattern = @"<script>(.*?)</script>";
+                            MatchCollection matches = Regex.Matches(content, pattern);
+
+                            if (matches.Count > 0)
+                            {
+                                string resultString = matches[matches.Count - 1].Groups[1].Value;
+                                resultString = Regex.Match(resultString, @"\[.*\]").Value;
+
+                                List<object>? listDetail = JsonConvert.DeserializeObject<List<object>>(resultString);
+
+                                listDetail = JsonConvert.DeserializeObject<List<object>>(listDetail.ElementAt(4).ToString());
+
+                                foreach (var item in listDetail)
+                                {
+                                    List<object> list = JsonConvert.DeserializeObject<List<object>>(item.ToString());
+                                    string goalType = list.ElementAt(0).ToString();
+                                    string firstHalfScore = list.ElementAt(4).ToString();
+                                    string fullTimeScore = list.ElementAt(4).ToString();
+                                    string code = list.ElementAt(5).ToString();
+
+                                    SBOMatchDetailModel model = new SBOMatchDetailModel(eventId, goalType, firstHalfScore, fullTimeScore, code);
+                                    sBOMatchDetailModels.Add(model);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            return sBOMatchDetailModels;
         }
+
     }
 }
