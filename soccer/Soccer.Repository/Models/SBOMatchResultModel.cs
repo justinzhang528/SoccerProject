fae4923b68cfc3d1eb22028ceb435644d668657a
justin <justinzhang515@gmail.com>
2023-10-11 16:18:05 +0800

    Added SBOMatchResultModel class

---
+++ soccer/Soccer.Repository/Models/SBOMatchResultModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Soccer.Repository.Models
+{
+    public class SBOMatchResultModel
+    {
+        public string Id { get; set; }
+        public string League { get; set; }
+        public string TeamA { get; set; }
+        public string TeamB { get; set; }
+        public string DateTime { get; set; }
+        public string FirstHalfScore { get; set; }
+        public string FullTimeScore { get; set; }
+        public string IsShowMoreData { get; set; }
+
+        public SBOMatchResultModel() { }
+
+        public SBOMatchResultModel(string id, string league, string teamA, string teamB, string dateTime, string firstHalfScore, string fullTimeScore, string isShowMoreData) 
+        {
+            Id = id;
+            League = league;
+            TeamA = teamA;
+            TeamB = teamB;
+            DateTime = dateTime;
+            FirstHalfScore = firstHalfScore;
+            FullTimeScore = fullTimeScore;
+            IsShowMoreData = isShowMoreData;
+        }
+    }
+}
