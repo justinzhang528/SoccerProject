074be831d62fe4bee2f73acb2574b845e2b2682d
justin <justinzhang515@gmail.com>
2023-10-12 18:03:15 +0800

    Added new table

---
+++ soccer/MatchResultDb/dbo/Tables/SBOMatchDetailHistory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SBOMatchDetailHistory]
+(
+	Id varchar(80) NOT NULL,
+	MarketType varchar(100) NOT NULL,
+	HomeFirstHalfScore varchar(3),
+	AwayFirstHalfScore varchar(3),
+	HomeFullTimeScore varchar(3),
+	AwayFullTimeScore varchar(3),
+	Code int NOT NULL,
+	UpdateTime DATETIME
+)
