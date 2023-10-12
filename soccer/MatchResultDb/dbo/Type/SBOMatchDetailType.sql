4a45d35c53d111e730586d7c929b261940f1139e
justin <justinzhang515@gmail.com>
2023-10-12 09:54:19 +0800

    Added types

---
+++ soccer/MatchResultDb/dbo/Type/SBOMatchDetailType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE [dbo].[SBOMatchDetailType] AS TABLE
+(
+	Id varchar(80) NOT NULL PRIMARY KEY,
+	MarketType varchar(100) NOT NULL,
+	HomeFirstHalfScore varchar(3),
+	AwayFirstHalfScore varchar(3),
+	HomeFullTimeScore varchar(3),
+	AwayFullTimeScore varchar(3),
+	Code int NOT NULL,
+	UpdateTime DATETIME
+)
