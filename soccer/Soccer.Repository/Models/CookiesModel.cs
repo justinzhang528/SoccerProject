2af39cc777b5b4f9a7210a37dc416fea8846b44d
justin <justinzhang515@gmail.com>
2023-10-14 18:27:27 +0800

    Added new model for sbo cookies

---
+++ soccer/Soccer.Repository/Models/CookiesModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Soccer.Repository.Models
+{
+    public class CookiesModel
+    {
+        public string Website { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Path { get; set; }
+        public string Domain { get; set; }
+
+        public CookiesModel() { }
+
+        public CookiesModel(string website, string name, string value, string path, string domain)
+        {
+            Website = website;
+            Name = name;
+            Value = value;
+            Path = path;
+            Domain = domain;
+        }
+    }
+}
